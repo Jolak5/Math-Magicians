b6241fbbbff0fec95f164274054a56ad3298e064
jolak5 <kayodej4@gmail.com>
2023-03-21 21:12:52 +0100

    Update calculator js to make all calculations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,66 +1,82 @@
+import { useState } from 'react';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 
 export default function Calculator() {
+  const [intialValue, finalValue] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    const result = calculate(intialValue, e.target.textContent);
+    finalValue((intialValue) => ({
+      ...intialValue,
+      ...result,
+    }));
+  };
+
+  const { total, next } = intialValue;
+
   return (
     <div className="whole">
-      <div className="OutputValue">0</div>
+      <div className="OutputValue">{next || total || 0}</div>
       <div className="calculatorContainer">
-        <button className="individualButton" onClick={calculate} type="button">
+        <button name="AC" className="individualButton" onClick={handleClick} type="button">
           AC
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" onClick={handleClick} type="button">
           +/-
         </button>
-        <button className="individualButton" onClick={operate} type="button">
+        <button className="individualButton" onClick={handleClick} type="button">
           %
         </button>
-        <button className="individualButton coloredButtons" onClick={operate} type="button">
+        <button className="individualButton coloredButtons" onClick={handleClick} type="button">
           ÷
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           7
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           8
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           9
         </button>
-        <button className="individualButton coloredButtons" onClick={operate} type="button">
+        <button className="individualButton coloredButtons" onClick={handleClick} type="button">
           x
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           4
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           5
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           6
         </button>
-        <button className="individualButton coloredButtons" onClick={operate} type="button">
+        <button className="individualButton coloredButtons" onClick={handleClick} type="button">
           -
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           1
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           2
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" type="button" onClick={handleClick}>
           3
         </button>
-        <button className="individualButton coloredButtons" onClick={operate} type="button">
+        <button className="individualButton coloredButtons" onClick={handleClick} type="button">
           +
         </button>
-        <button className="individualButton fullWidth" onClick={calculate} type="button">
+        <button className="individualButton fullWidth" type="button" onClick={handleClick}>
           0
         </button>
-        <button className="individualButton" onClick={calculate} type="button">
+        <button className="individualButton" onClick={handleClick} type="button">
           .
         </button>
-        <button className="individualButton coloredButtons" onClick={operate} type="button">
+        <button className="individualButton coloredButtons" onClick={handleClick} type="button">
           =
         </button>
       </div>
