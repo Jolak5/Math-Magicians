bc36ebcda7495ff1f586ef54540289ab301901e2
jolak5 <kayodej4@gmail.com>
2023-03-22 09:28:39 +0100

    fix linters

--- src/components/quote.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-function GetData() {
-  fetch('https://api.api-ninjas.com/v1/quotes?category=happiness');
-  const res = res.json();
-  return res;
-}
